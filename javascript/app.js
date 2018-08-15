c47793bdecb4cb5d928f57f7011bca463381d84b
dustin-longenecker <dl-dev@outlook.com>
2018-08-14 20:46:53 -0600

    all functionality working except favorites section will only link 1st in array

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,6 @@
-var topics = ["Stag", "Wolf", "Dog", "Rabbit"];
-
+var topics = ["Tiger", "Wolf", "Dog", "Rabbit"];
+var favorites = [];
+var responses = [];
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 function displayTopicInfo() {
 
@@ -20,13 +21,17 @@ function displayTopicInfo() {
       // Only taking action if the photo has an appropriate rating
       if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
         // Creating a div with the class "item"
+        responses.push(results[i]);
         var gifDiv = $("<li class='item'>");
         // Storing the result item's rating
         var rating = results[i].rating;
         var title = results[i].title;
         // Creating a paragraph tag with the result item's rating
         var p = $("<p>").text("Rating: " + rating);
-        var t = $("<p>").text("Title: ") + title;
+        var t = $("<p>").text("Title: " + title);
+        var b = $("<button>").text("Add Favorite");
+        b.addClass("favorite");
+        b.attr("data-id", results[i].id)
         // Creating an image tag
         var personImage = $("<img>");
         personImage.addClass("gif");
@@ -40,9 +45,10 @@ function displayTopicInfo() {
         $("#topics-view").prepend(gifDiv);
         //
         // // Appending the paragraph and personImage we created to the "gifDiv" div we created
-        gifDiv.append(title);
-        gifDiv.append(p);
         gifDiv.append(personImage);
+        gifDiv.append(t);
+        gifDiv.append(p);
+        gifDiv.append(b);
         // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
       }
     }
@@ -76,21 +82,7 @@ function renderButtons() {
     $("#topic-input").val('')
   }
 }
-
-// This function handles events where the add movie button is clicked
-//add animal on click "add animal"
-$("#add-topic").on("click", function(event) {
-  event.preventDefault();
-  // This line of code will grab the input from the textbox
-  var topic = $("#topic-input").val().trim();
-  // The movie from the textbox is then added to our array
-  topics.push(topic);
-  // Calling renderButtons which handles the processing of our movie array
-  renderButtons();
-});
-
-//gif still vs animate click function
-$(document).on("click", ".gif", function() {
+function animateGif() {
   var state = $(this).attr("data-state");
   if(state === "still"){
     $(this).attr("src", $(this).attr("data-animate"));
@@ -100,10 +92,47 @@ $(document).on("click", ".gif", function() {
     $(this).attr("src", $(this).attr("data-still"));
     $(this).attr("data-state", "still");
   }
-});
+}
+function addTopic(event) {
+  event.preventDefault();
+  // This line of code will grab the input from the textbox
+  var topic = $("#topic-input").val().trim();
+  // The movie from the textbox is then added to our array
+  topics.push(topic);
+  // Calling renderButtons which handles the processing of our movie array
+  renderButtons();
+}
 
+
+// This function handles events where the add movie button is clicked
+//add animal on click "add animal"
+$("#add-topic").on("click", addTopic);
+$(document).on("click", ".favorite", function(){
+  $("#favorites-view").empty();
+
+  var that = $(this).attr("data-id");
+  console.log(responses);
+  var ind = responses.indexOf(that, 0);
+  console.log(ind);
+  var ths = responses.slice(ind);
+  console.log(ths[0]);
+  favorites.push(ths[0]);
+
+  for (var i = 0; i < favorites.length; i++) {
+    var gifDiv = $("<li class='item'>");
+    $("#favorites-view").prepend(gifDiv);
+    var personImage = $("<img>");
+    personImage.addClass("gif");
+    // Giving the image tag an src attribute of a proprty pulled off the
+    // result item
+    personImage.attr("src", favorites[i].images.fixed_height_still.url);
+    gifDiv.append(personImage);
+  }
+  console.log(favorites);
+});
+//gif still vs animate click function
+$(document).on("click", ".gif", animateGif);
 // Adding click event listeners to all elements with a class of "movie"
 $(document).on("click", ".topic", displayTopicInfo);
-
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
